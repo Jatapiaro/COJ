be42f1d89c3df518510404820e3ff4e3cf158e97
jacobotapia <jacob.et.cetera@gmail.com>
2017-02-10 16:05:36 -0600

    Flag of unsolve

--- Level3/COJ2188.java
+++ Level3/COJ2188.java
@@ -1,20 +1,22 @@
 import java.util.Scanner;
+///Unsolved///
 public class COJ2188{
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 
+
 		int casos = sc.nextInt();
 
 		for(int i=0;i<casos;i++){
 
-			int zeros = sc.nextInt();
-			int ones = sc.nextInt();
-			int score = 0;
+			long zeros = sc.nextLong();
+			long ones = sc.nextLong();
+			long score = 0;
 
 			if(zeros>=ones){
-				int leftOnes = 0;
-				int rightOnes = 0;
+				long leftOnes = 0;
+				long rightOnes = 0;
 				while(ones>0){
 					if(ones>0){
 						leftOnes++;
@@ -29,9 +31,34 @@ public class COJ2188{
 				score-=(rightOnes*rightOnes);
 				score+=(zeros*zeros);
 				System.out.println(score);
+			}else if(ones-zeros==1){
+				System.out.println(-1);
 			}else{
-				ones-=zeros;
-				System.out.println((score-(ones*ones)));
+				int[] newResult = new int[(int)zeros+1];
+				int arrPos = 0;
+
+				while(ones>0){
+					newResult[arrPos]+=1;
+					ones--;
+					arrPos++;
+					if(arrPos==newResult.length){
+						arrPos=0;
+					}
+				}
+
+				score = 0;
+				for(int j=0;j<newResult.length;j++){
+					if(zeros>0){
+						score-=(((long)newResult[j]*(long)newResult[j]));
+						score+=1;
+						zeros--;
+					}else{
+						score-=((long)newResult[j]*(long)newResult[j]);
+					}
+				}
+
+				System.out.println(score);
+
 			}
 
 		}
