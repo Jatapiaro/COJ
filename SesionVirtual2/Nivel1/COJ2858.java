dd035f13091d2613300d0a2808b8ce6b6c2bc6be
jacobotapia <jacob.et.cetera@gmail.com>
2017-02-11 01:03:23 -0600

    Sols

--- SesionVirtual2/Nivel1/COJ2858.java
+++ SesionVirtual2/Nivel1/COJ2858.java
@@ -3,16 +3,28 @@ import java.math.BigInteger;
 public class COJ2858{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
+		BigInteger mod = new BigInteger("1000000007");
+		/*String a = "0101011101010101";
+		String l = "347923498234";
+		System.out.println(a.matches("[01]+")+" - "+l.matches("[01]+"));*/
 		while(sc.hasNext()){
 			String s = sc.nextLine();
 			String onlyStrings = s.replaceAll("[^A-Za-z]","*");
-			String onlyDigits = s.replaceAll("\\D","*");
-			String[] theNumbers = onlyDigits.split("\\*");
-			//System.out.println(onlyStrings+"\n"+onlyDigits);
+			String onlyDigits = s.replaceAll("\\D"," ");
+			String[] theNumbers = onlyDigits.replaceAll("[ ]+", " ").trim().split("\\s");
+			String onlyLetters = onlyStrings.replaceAll("[*]+", "*");
+			//System.out.println(onlyLetters);
 			for(String str:theNumbers){
-				System.out.println(str);
+				if(str.matches("[01]+")){
+					BigInteger b = new BigInteger(str,2);
+					BigInteger r = b.mod(mod);
+					//System.out.println(r);
+					onlyLetters = onlyLetters.replaceFirst("\\*",r.toString());
+				}else{
+					onlyLetters = onlyLetters.replaceFirst("\\*",str);
+				}
 			}
+			System.out.println(onlyLetters);
 		}
 	}
 }
