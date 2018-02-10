390b1fec9db8134e9aae54228ac583bf45f4cc3b
Jacobo <jacob.et.cetera@gmail.com>
2018-02-10 01:25:40 -0600

    Resuelve rooms

--- TopTec2018/SesionVirtual1/COJ1169.c
+++ TopTec2018/SesionVirtual1/COJ1169.c
@@ -1,5 +1,63 @@
 #include <stdio.h>
 
+int roomWithSpace( int rooms[], int nrooms ) {
+	int i;
+	for ( i = 0; i < nrooms; i++ ) {
+		if ( rooms[i] == 1 ) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+void addTwo( int rooms[], int * currentRoom, int nrooms ) {
+
+	int curr = *currentRoom;
+	if ( curr < nrooms ) {
+
+		if ( rooms[curr] == 0 ) {
+			rooms[curr] = 2;
+			curr++;
+			*currentRoom = curr;
+		} else {
+			rooms[roomWithSpace(rooms, nrooms)] = 2;
+			rooms[roomWithSpace(rooms, nrooms)] = 2;
+		}
+
+	} else {
+		curr++;
+		*currentRoom = curr;
+		rooms[roomWithSpace(rooms, nrooms)] = 2;
+		rooms[roomWithSpace(rooms, nrooms)] = 2;
+	}
+
+}
+
+void addOne( int rooms[], int * currentRoom, int nrooms ) {
+	int curr = *currentRoom;
+	if ( curr < nrooms ) {
+
+		if ( rooms[curr] == 0 ) {
+			rooms[curr] = 1;
+			curr++;
+			*currentRoom = curr;
+		} else {
+			rooms[roomWithSpace(rooms, nrooms)] = 2;
+		}
+
+	} else {
+		curr++;
+		*currentRoom = curr;
+		rooms[roomWithSpace(rooms, nrooms)] = 2;
+	}
+}
+
+void printArray( int rooms[], int nrooms ) {
+	for ( int i = 0; i<nrooms; i++ ) {
+		printf("%d\n", rooms[i]);
+	}
+}
+
 int main(int argc, char const *argv[]) {
 
 	int nrooms, gguest;
@@ -18,9 +76,23 @@ int main(int argc, char const *argv[]) {
 		int group;
 		scanf("%d", &group);
 
-		int rooms = group/2;
-		
+		int nr = group/2;
+		if ( group%2 == 0 ) {
+
+			for ( int k = 0; k<nr; k++) {
+				addTwo(rooms, &currentRoom, nrooms);
+			}
+
+		} else {
+			for ( int k = 0; k<nr; k++) {
+				addTwo(rooms, &currentRoom, nrooms);
+			}
+			addOne(rooms, &currentRoom, nrooms);
+		}
+		//printf("%d\n", currentRoom);
 
 	}
+
+	printArray(rooms, nrooms);
 
 }
